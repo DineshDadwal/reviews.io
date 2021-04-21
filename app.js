16a92132c7c0ac29f7e9667e76957ddee65e767d
DineshDadwal <dinesh.dadwal999@gmail.com>
2021-04-22 01:14:38 +0530

    first commit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
+    name: "Dinesh Dadwal",
     job: "web developer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      "/IMG_20201129_020500_185.jpg",
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
