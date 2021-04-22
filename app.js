bdcc17c477f8003b0bf5a0e837207fccf40d3f7e
Dinesh Dadwal <52106986+DineshDadwal@users.noreply.github.com>
2021-04-22 23:34:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const reviews = [
     name: "Dinesh Dadwal",
     job: "web developer",
     img:
-      "/IMG_20201129_020500_185.jpg",
+      "./IMG_20201129_020500_185.jpg",
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
